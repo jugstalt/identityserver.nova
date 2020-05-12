dbd2ba258b913592aadf802af3bc7b62ab99e71c
jugstalt <gstalt@gmx.at>
2020-05-12 19:55:54 +0200

    styling

---
+++ is4/IdentityServer/wwwroot/js/is4-legacy-ui.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+
+    $('.collapsable-tool').each(function (i, collapsable) {
+        var $collapsable = $(collapsable);
+
+        $collapsable.first('h4').click(function (e) {
+            console.log(this);
+            e.stopPropagation();
+            $collapsable.toggleClass('expanded');
+        });
+    });
+
+});
